1f066b1c87e96d0f0e7d43aeb7c1d9fbd33a3fdb
Aleksandra Soboleva <sandrasoboleva@gmail.com>
2022-02-15 06:30:27 -0500

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Movie = require("../models/Movie.model");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+router.get("/movies", function (req, res, next) {
+  Movie.find().then((results) => {
+    res.render("movies",{results: results});
+    console.log("got all movies");
+  });
+});
+
+router.get("/oneMovie/:one", function (req, res, next) {
+  Movie.findById(req.params.one).then((results) => {
+    res.render("oneMovie", results);
+    console.log("worked showing one movie", results);
+  });
+});
 
 module.exports = router;
